411d8737cd72c7004065b1ff21cd90d49bf554ae
Hyunkuk <boris0815@naver.com>
2023-03-19 20:58:17 +0900

    Update AppComponent.kt

--- app/src/main/java/com/example/android/dagger/di/AppComponent.kt
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -2,9 +2,7 @@ package com.example.android.dagger.di
 
 import android.content.Context
 import com.example.android.dagger.login.LoginComponent
-import com.example.android.dagger.main.MainActivity
 import com.example.android.dagger.registration.RegistrationComponent
-import com.example.android.dagger.settings.SettingsActivity
 import com.example.android.dagger.user.UserManager
 import dagger.BindsInstance
 import dagger.Component
@@ -28,5 +26,4 @@ interface AppComponent {
     fun registrationComponent(): RegistrationComponent.Factory
 
     fun loginComponent(): LoginComponent.Factory
-    /*
 }
